fe135d13b27260e355abf26f120b0af7f273eace
admin <admin@admin-PC>
2023-10-25 12:37:15 +0530

    first commit

---
+++ src/gitdemoproject/gitdemoProject.java
@@ -0,0 +1,18 @@
+package gitdemoproject;
+
+import java.util.Scanner;
+
+public class gitdemoProject {
+
+	public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+    	
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        System.out.println("You entered " + number);
+
+        // closing the scanner object
+        input.close();
+	}
+
+}
